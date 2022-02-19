a80b31e95cf984ae896a784d4852494eaaed6739
Aman Nanda <amannanda8@gmail.com>
2022-02-19 19:15:17 +0530

    metaweather client complete

--- weather/packages/meta_weather_api/lib/meta_weather_api.dart
+++ weather/packages/meta_weather_api/lib/meta_weather_api.dart
@@ -1,7 +1,4 @@
 library meta_weather_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/models.dart';
+export 'src/meta_weather_api_client.dart';
---
+++ weather/packages/meta_weather_api/test/src/models/location_test.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta_weather_api/src/models/location.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Location fromJson', () {
+    test('throws CheckedFromJsonException when enum is unknown', () {
+      expect(
+        () => Location.fromJson(<String, dynamic>{
+          'title': 'mock-title',
+          'location_type': 'Unknown',
+          'latt_long': '-34.75,83.28',
+          'woeid': 42,
+        }),
+        throwsA(isA<CheckedFromJsonException>()),
+      );
+    });
+  });
+}
